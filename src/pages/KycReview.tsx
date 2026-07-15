ff89251e79ec0757e3f7f28595b3603c6f64ebb3
Bland Irony <blandirony@gmail.com>
2026-07-15 09:10:24 +0100

    :sparkles: Initialized project, setup tailwind utilities and setup routes and layouts.

---
+++ src/pages/KycReview.tsx
@@ -0,0 +1,5 @@
+export default function KycReview() {
+  return (
+    <div>KycReview</div>
+  );
+}
---
+++ src/pages/MasterWallet.tsx
@@ -0,0 +1,5 @@
+export default function MasterWallet() {
+  return (
+    <div>MasterWallet</div>
+  );
+}
